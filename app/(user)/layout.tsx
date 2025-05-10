4e49fff4a08f214a3e2a5a476c6bfffb7e89b32b
Sam Dorra <islam.abderrezak@ensia.edu.dz>
2025-05-10 21:24:37 +0100

    fixed layout grid bug

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -6,7 +6,7 @@ export default function UserLayout({ children }: { children: ReactNode }) {
   return (
     <div className="grid min-h-screen w-screen grid-flow-row grid-rows-[auto_auto_1fr_auto] px-8 py-1 md:px-12 lg:px-16 xl:px-20">
       <NavBar />
-      {children}
+      <div>{children}</div>
       <Footer />
     </div>
   );
