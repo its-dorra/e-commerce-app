61c5446b10f062aceef749168ba282e5eb02de10
Sam Dorra <islam.abderrezak@ensia.edu.dz>
2025-05-10 21:17:20 +0100

    fixed layout grid bug

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -2,13 +2,12 @@ import NavBar from "@/lib/components/Navbar";
 import Footer from "@/lib/components/Footer";
 import { ReactNode } from "react";
 
-
-export default function UserLayout({children} : {children : ReactNode})
-{
-  return <div
-    className="grid min-h-screen w-screen grid-rows-[auto_auto_1fr_auto] px-8 py-1 md:px-12 lg:px-16 xl:px-20">
-    <NavBar />
-    {children}
-    <Footer />
-  </div>
+export default function UserLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="grid min-h-screen w-screen grid-flow-row grid-rows-[auto_auto_1fr_auto] px-8 py-1 md:px-12 lg:px-16 xl:px-20">
+      <NavBar />
+      {children}
+      <Footer />
+    </div>
+  );
 }
